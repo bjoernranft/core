0d3894eaac20dcda68ca88c3f89e3b6f591ab983
Michael Stahl <Michael.Stahl@cib.de>
2018-10-19 15:46:27 +0200

    sw_redlinehide_3: apparently a live SwNodeNum always has a parent
    ... so assert that somewhere.
    
    Change-Id: I928c55ffa57ec037e9406618d20b454e11dd80d4

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -4335,6 +4335,8 @@ void SwTextNode::RemoveFromListRLHidden()
 
 bool SwTextNode::IsInList() const
 {
+    // it looks like an unconnected Num can't happen, except in the undo-array
+    assert(!GetNum() || GetNum()->GetParent() || !GetNodes().IsDocNodes());
     return GetNum() != nullptr && GetNum()->GetParent() != nullptr;
 }
 
